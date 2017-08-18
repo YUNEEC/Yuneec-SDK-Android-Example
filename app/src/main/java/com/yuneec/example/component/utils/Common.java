4419472630bb97e27692d604d2a37e58f3d48926
Sushma Sathyanarayana <sush.satyan@gmail.com>
2017-08-18 16:56:35 -0700

    Added Telemetry listeners and other UI changes

--- app/src/main/java/com/yuneec/example/component/utils/Common.java
+++ app/src/main/java/com/yuneec/example/component/utils/Common.java
@@ -8,14 +8,22 @@ public
 class Common
 {
 
-	 public static final String VideoStreamUrl = "rtsp://192.168.42.1/live";
+   public static final String VideoStreamUrl = "rtsp://192.168.42.1/live";
 
-	 public static final int fixedRotationAngle = 20;
+   public static final int fixedRotationAngle = 20;
 
-	 public static int currentRotation = 0;
+   public static int currentRotation = 0;
 
-	 public static final String connectionStatusDefault = "Not connected to the drone";
+   public static final String connectionStatusDefault = "Not connected to the drone";
 
-	 public static String connectionStatus = connectionStatusDefault;
+   public static final String batteryStatusDefault = "Unknown";
+
+   public static final String healthStatusDefault = "Unknown";
+
+   public static String connectionStatus = connectionStatusDefault;
+
+   public static String healthStatus  = healthStatusDefault;
+
+   public static String batteryStatus = batteryStatusDefault;
 
 }
