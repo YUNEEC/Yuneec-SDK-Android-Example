2a65ebd9d437118c3fdbeeb66fa8c3227c2a1637
Sushma Sathyanarayana <sush.satyan@gmail.com>
2017-10-30 17:43:45 -0700

    Clear old mission items before adding new ones

--- MissionDemo/src/main/java/com/yuneec/example/component/activity/MainActivity.java
+++ MissionDemo/src/main/java/com/yuneec/example/component/activity/MainActivity.java
@@ -334,6 +334,7 @@ public class MainActivity
     private void enableDisableAdd() {
         if (isAdd == false) {
             isAdd = true;
+            missionItems.clear();
             add.setText("Done");
         } else {
             isAdd = false;
